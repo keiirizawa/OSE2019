b2e3e96577d70be8b3aebae56cdd24834d85d599
Kei Irizawa <kirizawa@uchicago.edu>
2019-07-29 18:25:29 -0500

    changes

--- oselab/DP_MultComms/submit_midway_cpu_scaling.sh
+++ oselab/DP_MultComms/submit_midway_cpu_scaling.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=8
 #SBATCH --job-name=DP_scale
 #SBATCH --cpus-per-task=28
-#SBATCH --output=DP_scale.out
+#SBATCH --output=DP_cpu_scale.out
 
 # this job will run in the broadwl partition on Midway
 #SBATCH --partition=broadwl
@@ -11,7 +11,7 @@
 module load openmpi
 
 
-for j in `seq 1 5; do
+for j in `seq 1 5`; do
     echo "This uses 10 MPI nodes and $j OMP threads:"
     export OMP_NUM_THREADS=$j
     mpirun ./VFI
