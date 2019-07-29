368047e7b91323132bff3588d4ca164d26cfde4c
Kei Irizawa <kirizawa@uchicago.edu>
2019-07-29 16:45:18 -0500

    changes

--- day4/supplementary_material/submit_midway.sh
+++ day4/supplementary_material/submit_midway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#SBATCH --ntasks= 12
 #SBATCH --job-name=pi_hybrid
 #SBATCH --cpus-per-task=8
 #SBATCH --output=pi_hybrid.out
@@ -10,14 +10,13 @@
 # load the openmpi default module
 module load openmpi
 
-localhost slots=25
 
 for i in `seq 1 3`; do
     for j in `seq 8 10`; do
         echo "This uses $i MPI processors and $j OMP threads:"
+        export SLURM_NTASKS=$i
         export OMP_NUM_THREADS=$j
-        #SBATCH --ntasks=$i
-        mpirun --hostfile hostfile -np $i ./pi_hybrid.exec
+        mpirun -np  ./pi_hybrid.exec
         echo "\n"
     done
 done
