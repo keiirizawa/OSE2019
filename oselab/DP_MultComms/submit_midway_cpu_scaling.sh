9d6dc4e708062ec49b25d08ec9445cbe49a8a41a
Kei Irizawa <kirizawa@midway2-0035.rcc.local>
2019-07-29 18:18:46 -0500

    changes

--- oselab/DP_MultComms/submit_midway_cpu_scaling.sh
+++ oselab/DP_MultComms/submit_midway_cpu_scaling.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH --ntasks=30
+#SBATCH --ntasks=8
 #SBATCH --job-name=DP_cpu_scale
 #SBATCH --cpus-per-task=28
-#SBATCH --output=DP_cpu_scale.out
+#SBATCH --output=DP_cpu2_scale.out
 
 # this job will run in the broadwl partition on Midway
 #SBATCH --partition=broadwl
