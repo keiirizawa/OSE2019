f5a22c30b2f0d5f13fe5deb5846d27f984107325
Kei Irizawa <kirizawa@uchicago.edu>
2019-07-29 16:39:58 -0500

    change

--- day4/supplementary_material/submit_midway.sh
+++ day4/supplementary_material/submit_midway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=pi_hybrid
-
+#SBATCH --cpus-per-task=8
 #SBATCH --output=pi_hybrid.out
 
 # this job will run in the broadwl partition on Midway
@@ -10,11 +10,14 @@
 # load the openmpi default module
 module load openmpi
 
+localhost slots=25
+
 for i in `seq 1 3`; do
     for j in `seq 8 10`; do
         echo "This uses $i MPI processors and $j OMP threads:"
         export OMP_NUM_THREADS=$j
-        mpirun -n $i ./pi_hybrid.exec
+        #SBATCH --ntasks=$i
+        mpirun --hostfile hostfile -np $i ./pi_hybrid.exec
         echo "\n"
     done
 done
