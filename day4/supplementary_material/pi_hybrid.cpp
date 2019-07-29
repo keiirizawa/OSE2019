c97f86f686d4138bb99d57a4a19ca278d2fe099f
Kei Irizawa <kirizawa@uchicago.edu>
2019-07-29 00:22:13 -0500

    changes

--- day4/supplementary_material/pi_hybrid.cpp
+++ day4/supplementary_material/pi_hybrid.cpp
@@ -21,14 +21,14 @@ int main(int argc, char *argv[]) {
 
     const double w = 1.0/double(num_steps);
     num = num_steps / size;
-    ri = rank / size;  // b/c interval [0,1]
+    //ri = rank / size;  // b/c interval [0,1]
 
     double time = -omp_get_wtime();
 
     #pragma omp parallel for reduction(+:sum)
     for(int i=0; i<num; ++i) {
-        double x = ri + (i+0.5)*w;
-        sum += 4.0/(1.0+x*x);
+        double x = (i + 0.5) * w;
+        sum += 4.0 / (1.0 + x * x);
     }
     pi = sum * w;
 
