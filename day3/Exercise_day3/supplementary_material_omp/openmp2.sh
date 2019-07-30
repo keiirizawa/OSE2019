4bc803338ed63c960e70dfc95e5b8400d53a1f48
Kei Irizawa <kirizawa@uchicago.edu>
2019-07-29 21:01:22 -0500

    changes

--- day3/Exercise_day3/supplementary_material_omp/openmp2.sh
+++ day3/Exercise_day3/supplementary_material_omp/openmp2.sh
@@ -2,18 +2,18 @@
 
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 
 #SBATCH --time=00:01:00
 
 #SBATCH --job-name=dot_prod
 #SBATCH --output=dot_prod.out
 #SBATCH --error=dot_prod.err
+#SBATCH --partition=broadwl
 
 for i in `seq 1 8`; do
     echo "This uses $i number of threads:"
     export OMP_NUM_THREADS=$i
-    ./dot_prod.exe
+    ./dot_prod.exec
 done
-
 
